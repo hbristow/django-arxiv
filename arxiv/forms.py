ae43b19f4aba8c9bab7bbc9066ccf185843efab3
= <=>
2015-06-04 11:09:35 -0400

    HTML5 form validation

--- arxiv/forms.py
+++ arxiv/forms.py
@@ -5,3 +5,8 @@ class SubscribeForm(forms.ModelForm):
     class Meta:
         model = models.Subscriber
         fields = ('email', 'subjects', 'timezone')
+        widgets = {
+            'email': forms.EmailInput(attrs={'required': True}),
+            'subjects': forms.SelectMultiple(attrs={'required': True}),
+            'timezone': forms.Select(attrs={'required': True})
+        }
